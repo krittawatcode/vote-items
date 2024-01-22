9f3639f0f2e6602d7e8812a39be15beebb1cf7ce
Kri Wis <dober@Kris-MacBook-Pro.local>
2024-01-22 21:52:38 +0700

    update git ignore

--- user-service/usecase/password.go
+++ user-service/usecase/password.go
@@ -4,7 +4,6 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
-	"strings"
 
 	"golang.org/x/crypto/scrypt"
 )
@@ -27,22 +26,4 @@ func hashPassword(password string) (string, error) {
 	hashedPW := fmt.Sprintf("%s.%s", hex.EncodeToString(shash), hex.EncodeToString(salt))
 
 	return hashedPW, nil
-}
-
-func comparePasswords(storedPassword string, suppliedPassword string) (bool, error) {
-	pwsalt := strings.Split(storedPassword, ".")
-
-	// check supplied password salted with hash
-	salt, err := hex.DecodeString(pwsalt[1])
-
-	if err != nil {
-		return false, fmt.Errorf("unable to verify user password")
-	}
-
-	shash, err := scrypt.Key([]byte(suppliedPassword), salt, 32768, 8, 1, 32)
-	if err != nil {
-		return false, err
-	}
-
-	return hex.EncodeToString(shash) == pwsalt[0], nil
 }
